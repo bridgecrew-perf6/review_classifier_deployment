17129aca8a73a50083489d9542b0076763860b72
pks98 <94729752+pks98@users.noreply.github.com>
2022-05-28 09:48:11 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#mkdir -p ~/.streamlit/
+#
+#echo "\
+#[server]\n\
+#port = $PORT\n\
+#enableCORS = false\n\
+#headless = true\n\
+#\n\
+#" > ~/.streamlit/config.toml
+
+#mkdir -p ~/.streamlit/
+#echo "[theme]
+#base='dark'
+#primaryColor='#c3baba'
+#[server]
+#headless = true
+#port = $PORT
+#enableCORS = false
+#" > ~/.streamlit/config.toml
+
+mkdir -p ~/.streamlit/
+echo "[theme]
+base="dark"
+textColor="#e88282"
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
+
+
+
+
